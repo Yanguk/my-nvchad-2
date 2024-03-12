5bb411afde1eff95c62b8dc209de5eeee0db46c3
Yanguk <yanguk.heo@knowre.com>
2024-03-12 12:17:11 +0900

    Add: 포멧터 설정

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,6 +3,19 @@ local options = {
 
   formatters_by_ft = {
     lua = { "stylua" },
+
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    tsx = { "prettier" },
+
+    racket = { "raco" },
+    sh = { "shfmt" },
+    rust = { "rustfmt" },
+    c = { "clang_format" },
+
+    toml = { "taplo" },
+    json = { "prettier" },
+    yaml = { "yamlfmt" },
   },
 }
 
