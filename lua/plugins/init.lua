9a801c473f4d9304437891c3b8cd48976a4eb76b
Yanguk <yanguk.heo@knowre.com>
2024-04-01 09:05:03 +0900

    Delete: Octo-nvim 삭제

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -410,22 +410,6 @@ return {
   },
 
   {
-    "pwntester/octo.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup({ enable_builtin = true })
-      vim.cmd([[hi OctoEditable guibg=none]])
-    end,
-    keys = {
-      { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" },
-    },
-  },
-
-  {
     "stevearc/aerial.nvim",
     cmd = { "AerialToggle" },
     config = true,
