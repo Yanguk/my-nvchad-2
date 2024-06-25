d94ddd54b930308083adaeb5095fab53f9dd3db3
Yanguk <yanguk@duck.com>
2024-06-25 16:58:27 +0900

    term option

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,17 +4,6 @@ M.ui = {
   theme = "gruvbox",
   -- transparency = true,
 
-  term = {
-    sizes = { sp = 0.3, vsp = 0.2 },
-    float = {
-      relative = "editor",
-      row = 0.02,
-      col = 0.048,
-      width = 0.905,
-      height = 0.9,
-    },
-  },
-
   nvdash = {
     load_on_startup = true,
   },
@@ -26,6 +15,17 @@ M.ui = {
     FoldColumn = {
       link = "UfoFoldedEllipsis",
     },
+  },
+}
+
+M.term = {
+  sizes = { sp = 0.3, vsp = 0.2 },
+  float = {
+    relative = "editor",
+    row = 0.02,
+    col = 0.048,
+    width = 0.905,
+    height = 0.9,
   },
 }
 
