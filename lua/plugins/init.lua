af478b8e723cdf31779cd1ff78ad17b7929adb6d
Yanguk <yanguk@duck.com>
2024-06-25 09:12:44 +0900

    Remove: rest nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -472,24 +472,6 @@ return {
   },
 
   {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-    },
-    config = true,
-  },
-
-  {
-    "rest-nvim/rest.nvim",
-    ft = "http",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-      require("rest-nvim").setup()
-    end,
-  },
-
-  {
     "lewis6991/gitsigns.nvim",
     opts = function()
       local nvchad_opts = require("nvchad.configs.gitsigns")
