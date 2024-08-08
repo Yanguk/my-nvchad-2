ce670035aefc28b65bbcea11f990956ebfdb8589
Yanguk <yanguk@duck.com>
2024-08-08 09:34:28 +0900

    Add: rest.nvim

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -45,4 +45,19 @@ return {
       },
     },
   },
+  pkg = {
+    enabled = true,
+    cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
+    -- the first package source that is found for a plugin will be used.
+    sources = {
+      "lazy",
+      "rockspec", -- will only be used when rocks.enabled is true
+      "packspec",
+    },
+  },
+  rocks = {
+    enabled = true,
+    root = vim.fn.stdpath("data") .. "/lazy-rocks",
+    server = "https://nvim-neorocks.github.io/rocks-binaries/",
+  },
 }
